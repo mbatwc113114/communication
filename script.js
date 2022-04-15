c18e7a4ff95628f9c009fe4c0f500a53a9c223bd
mbatwc113114 <95271461+mbatwc113114@users.noreply.github.com>
2022-04-15 18:15:40 +0530

    hgg

---
+++ script.js
@@ -0,0 +1,17 @@
+// encoding message
+
+// huffman algorithm
+functon two_min(list){
+    var a = list.MIN_VALUE ;
+    console.log(a)
+    var b = ;
+    
+
+}
+
+two_min([443,55,2,55,33])
+
+function encode(text){
+    Array encoded = {};
+    
+}
